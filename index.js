e86c6627179914985d3efa2177da57f1072290ca
hrsh-1294 <harshvashishth12@gmail.com>
2025-03-08 00:40:40 +0530

    Updates

--- index.js
+++ index.js
@@ -76,6 +76,7 @@ import { fileURLToPath } from "url";
 var __filename = fileURLToPath(import.meta.url);
 var __dirname = dirname(__filename);
 var vite_config_default = defineConfig({
+  base: "/portfolio/",
   plugins: [react(), runtimeErrorOverlay(), themePlugin()],
   resolve: {
     alias: {
